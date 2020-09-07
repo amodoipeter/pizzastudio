576cf0f36e82687aed5b6644c3c0269c244aa935
amodoipeter <amodoipeter@gmail.com>
2020-09-08 02:07:02 +0300

    Created a case sttement for the size of the pizza selected

--- js/script.js
+++ js/script.js
@@ -8,18 +8,7 @@ function Getpizza(name,size,crust,topping,total){
   this.total=total;
 }
 
-$(document).ready(function(){
-  $("button.proceed").click(function(event){
-   let pname = $(".name option:selected").val();
-   let psize = $("#size option:selected").val();
-   let pcrust = $("#crust option:selected").val();
-   let ptopping = [];
-   $.each($("input[name='toppings']:checked"), function(){
-       ptopping.push($(this).val());
-   });
-   console.log(ptopping.join(", "));
-
-   $(document).ready(function(){
+    $(document).ready(function(){
      $("button.proceed").click(function(event){
        let pname = $(".name option:selected").val();
        let psize=$("#size option:selected").val();
@@ -29,9 +18,26 @@ $(document).ready(function(){
          ptopping.push($(this).val());
        });
        console.log(ptopping.join(","));
+       case "0":
+       price =0;
+       break;
+       case "large":
+       price = 1200;
+       console.log(price);
+       break;
+       case "medium":
+       price = 850;
+       console.log("The price is "+price);
+       break;
+       case "small":
+       price = 600;
+       console.log(price);
+       default:
+       console.log("error");
+}
 
 
-     })
+});
 
 
 
